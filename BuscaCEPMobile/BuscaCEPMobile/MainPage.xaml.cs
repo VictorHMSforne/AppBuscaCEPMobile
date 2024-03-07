ad67e22e56d68b4d3d3a44f7fd58d546f9f951f1
Victor <vmendessforne@gmail.com>
2024-03-07 09:39:19 -0300

    Adicionar arquivos de projeto.

---
+++ BuscaCEPMobile/BuscaCEPMobile/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using BuscaCEPMobile.Servico;//chamei
+using BuscaCEPMobile.Servico.Modelo;//chamei também
+
+namespace BuscaCEPMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btCEP.Clicked += BuscarCEP;
+        }
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = txtCEP.Text.Trim();
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+                        lblResultado.Text = string.Format("Endereço: {2}, {3}, {0} {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                        txtCEP.Text = "";
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "O CEP " + cep + " não foi encontrado!", "OK");
+                    }
+                }
+                catch (Exception er)
+                {
+                    DisplayAlert("Erro Crítico", er.Message,"OK");
+                }
+            }
+        }
+        private bool isValidCep(string cep)
+        {
+            bool valido = true;
+            if (cep.Length!=8)
+            {
+                DisplayAlert("Erro", "CEP inválido! O CEP apenas oito números.", "OK");
+                valido = false;
+            }
+            int NovoCEP = 0;
+            if(!int.TryParse(cep, out NovoCEP)) //o ! é diferente
+            {
+                DisplayAlert("Erro", "O CEP contém apenas números.", "OK");
+                valido= false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ BuscaCEPMobile/BuscaCEPMobile/Servico/ViaCEPServico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using BuscaCEPMobile.Servico.Modelo; //add esse
+using Newtonsoft.Json;// e esse
+
+namespace BuscaCEPMobile.Servico
+{
+    public class ViaCEPServico
+    {
+        //Controler do MVC
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCEP(string cep) //os métodos státicos há necessidade de ter um retorno
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();//resumidamente abriu o navegador, de forma interna
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);// baixou a string
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);//de forma didática ele dezipa o conteúdo
+            if(end.cep==null)return null; 
+            return end;
+        }
+    }
+}
